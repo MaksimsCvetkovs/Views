796c4a36777c5028239e80ed1d4ad03bdc2e1ac4
Maxim <maximpixel@yandex.ru>
2022-10-18 07:04:25 +0300

    Init

---
+++ 2-suppliers.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suppliers</title>
+    <link rel="stylesheet" href="css/basic.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Suppliers</h1>
+        <div class="list-items">
+            <div class="list-item">
+                <img src="images/test-1.png" alt="images/test-1.png">
+                <div class="list-item-content">
+                    <h3>TEST1</h3>
+                </div>
+            </div>
+            <div class="list-item">
+                <img src="images/test-2.png" alt="images/test-2.png">
+                <div class="list-item-content">
+                    <h3>TEST2</h3>
+                </div>
+            </div>
+            <div class="list-item">
+                <img src="images/test-3.png" alt="images/test-3.png">
+                <div class="list-item-content">
+                    <h3>TEST3</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
